db30c1515e5c54e8afa1fe5c8fe9f30a42312a99
Fernando Obieta <fernando.obieta@blanktree.ch>
2016-10-25 11:36:23 +0200

    current time variable in the main class

--- novaspace.ino
+++ novaspace.ino
@@ -2,18 +2,22 @@
  * NOVASPACE
  * @author: Fernando Obieta - blanktree.ch
  * @date: 161025
- * @version: 0.8
+ * @version: 0.9
  */
 
+unsigned long currentTime;
 long distance;
 
 void setup() {
+	currentTime = 0;
 	distance = 100;
 
 	proximitySetup();
 }
 
 void loop() {
+
+	currentTime = millis();
 
 	proximityLoop();
 	
