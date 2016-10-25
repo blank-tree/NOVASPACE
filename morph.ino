a1aff8a64528ee33b9b36bdd254227e3894ac450
Fernando Obieta <fernando.obieta@blanktree.ch>
2016-10-25 13:48:25 +0200

    morph class with comments

--- morph.ino
+++ morph.ino
@@ -2,7 +2,7 @@
  * NOVASPACE - Morph
  * @author: Fernando Obieta - blanktree.ch
  * @date: 161025
- * @version: 0.3
+ * @version: 0.7
  */
 
 // Constants
@@ -12,22 +12,41 @@ const int LA_MAX = 2000;
 
 // Variables
 Servo linearactuator;
+int LAvalue;
 
 void morphSetup() {
 
 	// init linear actuator object
 	linearactuator.attach(PIN_LINEARACTUATOR, LA_MIN, LA_MAX);
+
+	// init variables
+	LAvalue = 0; // linear actuator value goes from 0 to 180
+
+	// position the linear actuator
+	morph();
 	
 }
 
 void morphLoop() {
 	
+	// increase or decrease the linear actuator value and make sure
+	// that the min of 0 and the max of 180 won't get overstepped 
+	if (direction) {
+		LAvalue = LAvalue + 2 > 180 ? 180 : LAvalue + 2;
+	} else {
+		LAvalue = LAvalue - 1 < 0 ? 0 : LAvalue - 1;
+	}
+
+	// position the linear actuator
+	morph();
 }
 
 /**
- * Function to morph the linearactuator in a certain direction
- * @param boolean direction     // true = move up; false = move down;
+ * Morph Function
+ * Writes the linear actuator value to the linear actuator
+ * also maps the value in the opposite direction (so that the
+ * code makes more sense)
  */
-void morphUp(boolean direction) {
-
+void morph() {
+	linearactuator.write(map(LAvalue, 0, 180, 180, 0));
 }
